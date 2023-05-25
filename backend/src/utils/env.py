e585afe3b74650100d36ba4a93c8b95b0ee7debc
domino659 <martin@sionfamily.com>
2023-05-26 01:16:40 +0200

    Moved backend structure to separate Dockerfile, requirement, log file from source code. Added logs and env file for DB.

---
+++ backend/src/utils/env.py
@@ -0,0 +1,14 @@
+import os
+from pydantic import BaseSettings
+
+# Pydantic BaseSetting, permet de declarer un mapping du `.env` .
+# En donnant le chemin d'acces au fichier .env, pydantic gère l'import des variables.
+
+class Settings(BaseSettings):
+    database_url: str
+
+    class Config:
+        environment = os.environ['ENVIRONMENT']
+        env_file = f"../env/{environment}.env"
+
+settings = Settings()
