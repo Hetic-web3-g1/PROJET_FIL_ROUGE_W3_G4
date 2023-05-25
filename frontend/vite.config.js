c7086c6e7980577c06e82b703a612573e9372b11
domino659 <martin@sionfamily.com>
2023-05-25 19:59:19 +0200

    Creation of the backend, db on docker. Adaptation of the frontend to docker. Creation of a developement and a production build. Still need testing on prod.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = process.env.PORT
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: isDevelopment,
+    },
+    host: true,
+    strictPort: true,
+    port: port, // Specify the desired port number here
+  },
 })
