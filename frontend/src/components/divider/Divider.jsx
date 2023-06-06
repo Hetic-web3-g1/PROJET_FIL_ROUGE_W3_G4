e3646bb2a1662764cb2241e01ae4e3a5b2effa20
Erzenkell <ferrier.sammy@gmail.com>
2023-06-06 12:35:46 +0200

    added divider component

---
+++ frontend/src/components/divider/Divider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './divider.css'
+
+export const Divider = ({text}) => {
+    return (
+        text ? <div className='divider'>{text}</div> : <div className='divider-no-text'/>
+    )
+}
+
+Divider.propTypes = {
+    text: PropTypes.string
+}
+
+export default Divider;
---
+++ frontend/src/components/divider/Divider.stories.js
@@ -0,0 +1,18 @@
+import { Divider } from './Divider';
+
+export default {
+  title: 'Divider',
+  component: Divider,
+  tags: ['autodocs'],
+};
+
+export const Text = {
+    args: {
+        text: 'Test Divider',
+    },
+};
+
+export const NoText = {
+    args: {
+    },
+};
