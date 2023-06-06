ac07ce37105111d0a7e5f2f95e1d14555521fb94
Erzenkell <ferrier.sammy@gmail.com>
2023-06-06 11:16:48 +0200

    added constant colors

---
+++ frontend/src/constants/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    NeutralWhite: '#FDFCFA',
+    OxfordBlue: '#020A33',
+    Selected: "#FFCF00",
+    GreyBlue: "#121525",
+    RoyalBlueDark: "#0C1A66",
+  };
+
+module.exports = Colors ;
