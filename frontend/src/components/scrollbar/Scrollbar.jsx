2f8b42fe3f93746e8e605d264157cb3248d6e68c
unknown <alexandre912323@gmail.com>
2023-06-07 10:55:02 +0200

    test

---
+++ frontend/src/components/scrollbar/Scrollbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './scrollbar.css';
+
+export const Scrollbar = ({ content }) => {
+  const lorem = content ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' : '';
+  return (
+    <div className={lorem ? 'scrollbar size' : 'scrollbar'}>
+      {lorem}
+    </div>
+  );
+};
+
+Scrollbar.propTypes = {
+  content: PropTypes.bool
+};
+
+Scrollbar.defaultProps = {
+  content: false
+};
+
+export default Scrollbar;
---
+++ frontend/src/components/scrollbar/Scrollbar.stories.js
@@ -0,0 +1,18 @@
+import { Scrollbar } from './Scrollbar';
+
+export default {
+  title: 'Scrollbar',
+  component: Scrollbar,
+  tags: ['autodocs']
+};
+
+export const Base = {
+  args: {
+  },
+};
+
+export const Content = {
+  args: {
+    content: true
+  },
+};
