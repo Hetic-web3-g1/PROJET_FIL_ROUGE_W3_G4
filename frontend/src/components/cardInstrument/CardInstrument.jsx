b903e3fa47fb2134f64f038096b72b07cec6309c
unknown <alexandre912323@gmail.com>
2023-06-08 15:55:29 +0200

    test

--- frontend/src/components/cardInstrument/CardInstrument.jsx
+++ frontend/src/components/cardInstrument/CardInstrument.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './cardInstrument.css';
 
 export const CardInstrument = ({ name, size, legend, onClick }) => {
   const mode = legend ? 'L-' : '';
+  const [style, setStyle] = useState(false);
+
+  /**
+   * Select all img element, replace the highlight by highlight-close class on all element to put the highlight class to the selected element.
+   */
+  function clickEvent() {
+    document.querySelectorAll("img").forEach(e => {
+      e.classList.replace('highlight', 'highlight-close');
+    });
+
+    setStyle(!style);
+    onClick(name);
+  }
+
   return (
-    <div>
-        <img className={`max-height`} height={size} src={`src/assets/cardInstrument/${mode}${name}.svg`} onClick={() => onClick(name)}/>
+    <div style={{padding: '10px'}}>
+        <img key={name} className={`img-card-instrument ${style ? 'highlight' : 'highlight-close'}`} height={size} id="first" src={`src/assets/cardInstrument/${mode}${name}.svg`} onClick={clickEvent}/>
     </div>
   );
 };
@@ -19,7 +33,7 @@ CardInstrument.propTypes = {
 };
 
 CardInstrument.defaultProps = {
-  name: 'Celio.svg',
+  name: 'Celio',
   legend: false,
   size: 100
 };
