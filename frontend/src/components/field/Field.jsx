65a3a55cb8e146c2d739db7bd372d838ff060898
Spudwebb96 <jonas.janko17@gmail.com>
2023-06-08 15:11:30 +0200

    Login finish + Field change

--- frontend/src/components/field/Field.jsx
+++ frontend/src/components/field/Field.jsx
@@ -5,21 +5,39 @@ import './field.css'
 
 const searchIcon = '../../assets/search/search.svg';
 
-export const Field = ({ type, placeholder, onChange }) => {
+export const Field = ({ type, placeholder, onChange, }) => {
+
+function HideAndShowPassword () {
+    var x = document.getElementsByClassName("field-input-password")[0]; 
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }}
+
+
     return (
-        <input
-            className={['field-input', `field-input-${type}`].join(' ')}
-            type={type}
-            placeholder={placeholder}
-            onChange={onChange}
-        />
+        <div className='input-wrap'>
+            <input
+                className={['field-input', `field-input-${type}`].join(' ')}
+                type={type}
+                placeholder={placeholder}
+                onChange={onChange}
+            />
+            <svg  
+                className={['field-icon', `field-icon-${type}`].join(' ')}
+                onClick={HideAndShowPassword}
+                />
+        </div>
     );
 }
+
 
 Field.propTypes = {
     type: propTypes.string,
     placeholder: propTypes.string.isRequired,
     onChange: propTypes.func,
+    image: propTypes.string,
 };
 
 Field.defaultProps = {
@@ -29,3 +47,5 @@ Field.defaultProps = {
 };
 
 export default Field;
+
+
