de35ee8d26f32c1464d368f34d992caf2b3f7ffc
Martin SION <47590798+domino659@users.noreply.github.com>
2023-06-13 14:48:16 +0000

    Fix

--- backend/src/users/service.py
+++ backend/src/users/service.py
@@ -13,14 +13,14 @@ def _parse_row(row: sa.Row):
     return User(**row._asdict())
 
 
-def create_user(conn: Connection, user: UserCreate, user_id: UUID) -> User:
+def create_user(conn: Connection, user: UserCreate) -> User:
     result = conn.execute(
         sa.select(user_table).where(user_table.c.email == user.email)
     ).first()
     if result is not None:
         raise EmailAlreadyExist
 
-    created_user = db_srv.create_object(conn, user_table, user.dict(), user_id=user_id)
+    created_user = db_srv.create_object(conn, user_table, user.dict())
     return _parse_row(created_user)
 
 
