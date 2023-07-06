6450724a0a4f3212e7bee9858ea94ba1ca4e8e8e
Erzenkell <ferrier.sammy@gmail.com>
2023-07-06 15:48:56 +0200

    save for storybook stash

--- frontend/src/components/upload/UploadCard.jsx
+++ frontend/src/components/upload/UploadCard.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import propTypes from 'prop-types'
+import React, {useState} from 'react'
 
 import './uploadCard.css'
 import Divider from '../divider/Divider'
-import Button from '../button/Button'
 
 export const UploadCard = ({  }) => {
+    const [drag, setDrag] = useState(false);
+
+    const handleDrag = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.type === "dragenter" || e.type === "dragover") {
+            setDrag(true);
+        } else if (e.type === "dragleave") {
+            setDrag(false);
+        }
+    }
+
     return (
-        <div className="upload-card">
+        <div className="upload-card" onDragEnter={handleDrag}>
             <div className="upload-card-content">
                 <div className="upload-card-drop">
                     Drop Here
