df1cb29b008199de66d1ae49fc90f8fe7daa00ef
Erzenkell <ferrier.sammy@gmail.com>
2023-07-10 11:48:24 +0200

    fix reset password oula

---
+++ frontend/src/pages/auth/reset/ResetPassword.jsx
@@ -0,0 +1,49 @@
+import React, {useState, useContext} from 'react';
+import './resetPassword.css';
+import Button from '../../../components/button/Button';
+import Field from '../../../components/field/Field';
+import { useNavigate } from "react-router-dom";
+
+export const ResetPassword = ({ }) => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+
+  const token = window.location.href.split('/')[4];
+
+  const resetForm = (e) => { 
+    if (password != confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+    else {
+      e.preventDefault();
+      const resetOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
+        body: JSON.stringify({password, token }),
+      };
+      fetch('http://localhost:4000/auth/reset-password', resetOptions).then((response) => response.json()).then(data => {
+        if (data?.detail != "Invalid Credentials" && data?.detail != "Expired Token") {
+          navigate("/login");
+        } else {
+          alert('Invalid Token');
+        }
+      });
+    }
+  };
+
+  return (
+    <div className="reset-wrap">
+      <form className="reset-form">
+          <label for="password" className='reset-field'>Password</label>
+          <Field type="password" placeholder="Enter your password" onChange={(e) => {setPassword(e.target.value)}}/>
+          <label for="password" className='reset-field' >Confirm Password</label>
+          <Field type="password" placeholder="Confirm your password" onChange={(e) => {setConfirmPassword(e.target.value)}}/>
+          <Button label="reset" className="button button-secondary padded" onClick={resetForm}/>
+      </form>
+    </div>
+  );
+}; 
+
+export default ResetPassword;
---
+++ frontend/src/pages/auth/reset/ResetPasswordEmail.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useContext} from 'react';
+import './resetPassword.css';
+import Button from '../../../components/button/Button';
+import Field from '../../../components/field/Field';
+import { useNavigate } from "react-router-dom";
+
+export const ResetPasswordEmail = ({ }) => {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const resetEmailForm = (e) => { 
+      e.preventDefault();
+      const emailOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
+      };
+      fetch(`http://localhost:4000/auth/forgot-password?email=${email}`, emailOptions).then((response) => response.json()).then(data => {
+        if (data.detail != "Invalid Credentials") {
+          navigate(`/reset-password/${data.token}`);
+        } else {
+          alert('Invalid Email');
+        }
+      });
+  };
+
+  return (
+    <div className="reset-wrap">
+      <form className="reset-form">
+          <label for="password" className='reset-field'>Email</label>
+          <Field type="email" placeholder="Enter your Email" onChange={(e) => {setEmail(e.target.value)}}/>
+          <Button label="reset" className="button button-secondary padded" onClick={resetEmailForm}/>
+      </form>
+    </div>
+  );
+}; 
+
+export default ResetPasswordEmail;
