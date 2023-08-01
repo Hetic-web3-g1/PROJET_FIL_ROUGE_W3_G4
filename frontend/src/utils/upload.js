61e91e450a12a906a6b1f4d568aafb5c70c552b2
Erzenkell <ferrier.sammy@gmail.com>
2023-08-02 01:26:38 +0200

    added upload function

---
+++ frontend/src/utils/upload.js
@@ -0,0 +1,20 @@
+export const uploadFile = (file, file_type, isPublic) => {
+    const uploadOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'accept': 'application/json', 'authorization': `${store.getState().user.user_token}` },
+        body : JSON.stringify({
+            file: file,
+            isPublic: isPublic,
+        }),
+    };
+
+    fetch(`http://localhost:4000/s3_objects/upload/${file_type}`, uploadOptions).then((response) => response.json()).then(data => {
+        if (data?.detail[0].msg != "field required") {
+            handleClose();
+        } else {
+            alert('Invalid data');
+        }
+    });
+};
+
+export default uploadFile;
