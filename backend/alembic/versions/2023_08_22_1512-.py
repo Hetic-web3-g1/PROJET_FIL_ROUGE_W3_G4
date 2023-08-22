3ded7b7adc919a59d50b7e8c1ced60690b79ee4e
domino659 <martin@sionfamily.com>
2023-08-22 17:15:48 +0200

    fixed using alembic

---
+++ backend/alembic/versions/2023_08_22_1512-.py
@@ -0,0 +1,24 @@
+"""Alembic merge heads
+
+Revision ID: cac9879d97b5
+Revises: 199f771aaa97
+Create Date: 2023-08-22 15:12:57.424065
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "cac9879d97b5"
+down_revision = "199f771aaa97"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
