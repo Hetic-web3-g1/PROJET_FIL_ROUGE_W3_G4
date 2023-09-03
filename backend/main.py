4857f8ba9689a00ae01cdffcade67c2bb98d955a
domino659 <martin@sionfamily.com>
2023-09-03 19:04:23 +0200

    fix docker bug + change env file for backend

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from config import settings
+from src.utils.fake_data import generate_data
 
 if settings.environment in {"development"}:
     origins = ["*"]
