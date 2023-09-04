97e971ea2ee4885974952193eae763db038045be
domino659 <martin@sionfamily.com>
2023-09-04 15:46:55 +0200

    cooooooorrrss

--- backend/src/database/s3_engine.py
+++ backend/src/database/s3_engine.py
@@ -1,4 +1,4 @@
-import boto3
+import boto3, json
 from botocore.utils import fix_s3_host
 
 from config import settings
@@ -13,3 +13,21 @@ s3_client.meta.events.unregister("before-sign.s3", fix_s3_host)
 
 # Create DB
 s3_client.create_bucket(Bucket=settings.bucket_name)
+
+cors_policy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowOrigin",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": ["s3:GetObject"],
+            "Resource": [f"arn:aws:s3:::{settings.bucket_name}/*"],
+        }
+    ],
+}
+
+
+cors_policy_json = json.dumps(cors_policy, indent=4)
+
+s3_client.put_bucket_policy(Bucket=settings.bucket_name, Policy=cors_policy_json)
