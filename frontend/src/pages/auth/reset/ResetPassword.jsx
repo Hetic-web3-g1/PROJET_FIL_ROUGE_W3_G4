5ca326efdde59f9ec46bdf238a530937893bf5fb
Erzenkell <ferrier.sammy@gmail.com>
2023-09-12 22:49:59 +0200

    service file for auth + removed relocated page

--- frontend/src/pages/auth/reset/ResetPassword.jsx
+++ frontend/src/pages/auth/reset/ResetPassword.jsx
@@ -1,39 +1,32 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import './resetPassword.css';
 import Button from '../../../components/button/Button';
 import Field from '../../../components/field/Field';
 import { useNavigate } from "react-router-dom";
 import { useToast } from '../../../utils/toast';
 
+import { resetPassword } from '../../../services/auth';
+
 export const ResetPassword = ({ }) => {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  
   const navigate = useNavigate();
   const toast = useToast();
 
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordData, setPasswordData] = useState(null);
   const token = window.location.href.split('/')[4];
 
   const resetForm = (e) => { 
-    if (password != confirmPassword) {
-      alert('Passwords do not match');
-      return;
-    }
-    else {
-      e.preventDefault();
-      const resetOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
-        body: JSON.stringify({password, token }),
-      };
-      fetch(`http://${import.meta.env.VITE_API_ENDPOINT}/auth/reset-password`, resetOptions).then((response) => response.json()).then(data => {
-        if (data?.detail != "Invalid Credentials" || data?.detail != "Expired Token") {
-          navigate("/login");
-        } else {
-          toast.open({message: 'Invalid Credentials', type: 'failure'});
-        }
-      });
-    }
+    e.preventDefault();
+    resetPassword(token, password, confirmPassword, toast, setPasswordData);
   };
+
+  useEffect(() => {
+    if(passwordData) {
+      navigate(`/login`);
+    }
+  }, [passwordData])
 
   return (
     <div className="reset-wrap">
